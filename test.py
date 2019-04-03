212e769cd055f5c8ac6bfaae28a44ff27048806f
Wan-Fang Chou <wan-fang.chou@cgu.edu>
2019-04-03 11:43:40 -0700

    Deleted previous tests files

--- test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase, Client
-from accounts.views import *
-
-#class LoginTestCase(TestCase):
-
-#    def test_login(self):
-        # First, check for the default behavior
-#        response = self.client.get('login')
-#        self.assertRedirects(response, )
