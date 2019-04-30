2863691de0b63b7e60278fc0bef06783c4648cec
Wan-Fang Chou <wan-fang.chou@cgu.edu>
2019-04-30 13:24:05 -0700

    Debug signup ValueError

--- accounts/views.py
+++ accounts/views.py
@@ -3,21 +3,26 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 
 def signup(request):
-    if request.method == 'POST':
-        # User has info and wants an account now!
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.get(username=request.POST['username'])
-                return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
-            except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-                auth.login(request,user)
-                return redirect('home')
-        else:
-            return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
-    else:
-        # User wants to enter info
-        return render(request, 'accounts/signup.html')
+    while True:
+        try:
+            if request.method == 'POST':
+                # User has info and wants an account now!
+                if request.POST['password1'] == request.POST['password2']:
+                    try:
+                        user = User.objects.get(username=request.POST['username'])
+                        return render(request, 'accounts/signup.html', {'error':'Username has already been taken'})
+                    except User.DoesNotExist:
+                        user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                        auth.login(request,user)
+                        return redirect('home')
+                else:
+                    return render(request, 'accounts/signup.html', {'error':'Passwords must match'})
+            else:
+                # User wants to enter info
+                return render(request, 'accounts/signup.html')
+
+        except ValueError:
+            return render(request, 'accounts/signup.html', {'error': 'You have to fill in your username and/or password'})
 
 def login(request):
     if request.method == 'POST':
